d098d5a7840cefde916832d17ac24a461c5f2c30
rogozhka <serge.rogozhkin@gmail.com>
2019-05-21 16:28:09 +0300

    Add url tests

--- wrap-detect_test.go
+++ wrap-detect_test.go
@@ -1,6 +1,7 @@
 package ytrwrap
 
 import (
+	"net/url"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -20,4 +21,30 @@ func TestTr_DetectEN(t *testing.T) {
 	lc, err := tr.Detect("the pony is eating grass", nil)
 	assert.Nil(t, err, "err")
 	assert.Equal(t, EN, lc, "lc")
+}
+
+func TestURLDetect(t *testing.T) {
+
+	dummyKey := "trnsl.there.is.no.key"
+	text2detect := "no-text"
+
+	client := newVoidClient()
+	tr := NewYandexTranslateWithClient(dummyKey, client)
+
+	_, apierr := tr.Detect(text2detect, nil)
+	assert.NotNil(t, apierr, "detect err")
+
+	baseURL, err := url.Parse(YandexTranslateAPI)
+	assert.Nil(t, err, "URL.Parse err")
+
+	theURL, err := url.Parse(client.LastURL())
+	assert.Nil(t, err, "URL.Parse err")
+	assert.Equal(t, baseURL.Host, theURL.Host, "url")
+	assert.Equal(t, baseURL.Path+"/tr.json/detect", theURL.Path, "url")
+
+	values := theURL.Query()
+
+	assert.Equal(t, dummyKey, values.Get("key"), "url")
+	assert.Equal(t, "", values.Get("hint"), "url")
+	assert.Equal(t, text2detect, values.Get("text"), "url")
 }
--- wrap-langs_test.go
+++ wrap-langs_test.go
@@ -1,6 +1,7 @@
 package ytrwrap
 
 import (
+	"net/url"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -8,10 +9,35 @@ import (
 
 func TestTr_Langs(t *testing.T) {
 
-	tr := createTestClientFromEnv()
+	tr := createRealTestClientFromEnv()
 
 	ll, err := tr.Langs("en")
 	assert.Nil(t, err, "err")
 	assert.NotNil(t, ll, "res")
 	assert.Equal(t, "English", ll["en"], "in map")
+}
+
+func TestURLLangs(t *testing.T) {
+
+	dummyKey := "trnsl.there.is.no.key"
+
+	client := newVoidClient()
+	tr := NewYandexTranslateWithClient(dummyKey, client)
+
+	uiLC := AF
+
+	_, apierr := tr.Langs(uiLC)
+	assert.NotNil(t, apierr, "Langs err")
+
+	baseURL, err := url.Parse(YandexTranslateAPI)
+	assert.Nil(t, err, "URL.Parse err")
+
+	theURL, err := url.Parse(client.LastURL())
+	assert.Nil(t, err, "URL.Parse err")
+	assert.Equal(t, baseURL.Host, theURL.Host, "url")
+	assert.Equal(t, baseURL.Path+"/tr.json/getLangs", theURL.Path, "url")
+
+	values := theURL.Query()
+	assert.Equal(t, dummyKey, values.Get("key"), "url")
+	assert.Equal(t, string(uiLC), values.Get("ui"), "url")
 }
